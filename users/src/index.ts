2e3b8f6d6d210b0acfc63a306a313152b375a377
Irineu Licks Filho <irineu@intodigital.com.br>
2019-10-27 16:51:17 -0300

    Lb4 app init

---
+++ users/src/index.ts
@@ -0,0 +1,16 @@
+import {UsersApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {UsersApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new UsersApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
